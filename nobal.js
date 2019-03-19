caaeb504c5476d9198e3876ea1c4c22631834452
rehabsaadoun <YOUR GITHUB EMAIL>
2019-03-19 07:29:56 +0300

    rehab

---
+++ nobal.js
@@ -0,0 +1,32 @@
+class NobleHouses{
+    constructor(HouseName, Sigil, Words, Enemies){
+   this.HouseName =HouseName;
+   this.sigil=sigil;
+   this.words= word;
+this.newEnemies = []
+    }
+   houseInfo(){
+var i =
+this.houseName.indexOf()
+   }
+   newEnemy(name){
+    var i = this.newEnemy.indexOf(this.Enemies);
+    console.log(this.newEneies[i+1])
+ }
+}
+
+  Class nobalCharacters{
+    constructor(Name.status, newEnemies)
+   this.charactersName = DaenerysTargaryen, CerseiLannister, jonSnow;
+   this.status= kill, revive;
+ 
+
+   kill(){
+var i = this.kill.indesxOf(Characters);
+console.log(this.Characters[i-1])
+   
+revive()
+var i = this.revive.indesxOf(Characters);
+console.log(this.Characters[i+1])
+  }}
+  var r = new newEnemies(" CerseiLannister")
