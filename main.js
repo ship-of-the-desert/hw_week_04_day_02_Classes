9ece486df478077c497e6b2284b2cec991a9bd8d
REEMA <reema.rayes@gmail.com>
2019-03-19 09:17:12 +0300

    hw_w4_d2

---
+++ main.js
@@ -0,0 +1,40 @@
+class Noble_Houses {
+    constructor(houseName, Sigil, Words, Enemies){
+      this.houseName = houseName;
+      this.Sigil = Sigil;
+      this.Words = Words;
+      this.Enemies = [];
+    }
+
+    houseInfo(){ //that displays the name, sigil and words of the house.
+        Noble_Houses(houseName){
+            // create 3 different houses
+            console.log()
+          }
+
+    }
+    //Add 'Jon Snow' to Cersei Lannister's enemy list
+    newEnemy(name){ //that adds a new enemy to the house.
+
+    }
+
+
+    class Character {
+        constructor(name, status){
+            this.name = name;
+            this.status = status;
+        }
+
+        kill() { //Kill 'Jon Snow'.
+
+        }
+
+        revive(){
+
+        }
+
+        Character(name){
+        this.name.push("Daenerys Targaryen", "Cersei Lannister", "Jon Snow");
+        
+        }
+    }
